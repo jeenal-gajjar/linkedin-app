d2f81d6e748efa8dade0e9abf829a2eb08ed8f67
jeenal <jeenal201suthar@gmail.com>
2019-12-03 16:05:57 +0530

    get method add

--- linkedin/views.py
+++ linkedin/views.py
@@ -5,11 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 class LinkedinApi(View):
+    def get(self, request):
+        pass
+
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(LinkedinApi, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        data = json.loads(request.body.decode("UTF-8"))
-        print(data)
-        return data
+        return
